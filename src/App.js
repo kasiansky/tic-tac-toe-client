cc217cf40edfb3096575267fd28f3c98fd26775a
kasiansky <happyhusband108gmail.com>
2022-08-14 11:41:52 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Components/Header/Header";
+import Main from "./Components/Main/Main";
+import Footer from "./Components/Footer/Footer";
+import JoinRoomModal from "./Components/JoinRoomModal/JoinRoomModal";
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:5000");
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [roomCode, setRoomCode] = useState(null);
+
+  useEffect(() => {
+    console.log(roomCode);
+    if (roomCode) {
+      socket.emit("joinRoom", roomCode);
+    }
+  }, [roomCode]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <JoinRoomModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        setRoomCode={setRoomCode}
+      />
+      <Header />
+      <Main socket={socket} roomCode={roomCode} />
+      <Footer setShowModal={setShowModal} />
+    </>
   );
 }
 
